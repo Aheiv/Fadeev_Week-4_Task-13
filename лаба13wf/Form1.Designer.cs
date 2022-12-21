f171a46b0c24dfef99dcf1ec15da52b40a5ee808
DaniilF2323@outlook.com <DaniilF2323@outlook.com>
2022-12-21 22:45:53 +0300

    Добавьте файлы проекта.

---
+++ лаба13wf/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace лаба13wf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(17, 120);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(717, 552);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(168, 48);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(399, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(313, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Поиск по дате";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(576, 47);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 692);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ лаба13wf/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace лаба13wf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Client> clientList = new List<Client>();
+            StreamReader sr = new StreamReader("ClientList.txt", Encoding.UTF8);
+            while (!sr.EndOfStream)
+            {
+                string[] s = sr.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                if (s.Length == 5)
+                {
+                    clientList.Add(new Creditor(s[0], Convert.ToDateTime(s[1]), Convert.ToDecimal(s[2]),
+                                                      Convert.ToDouble(s[3]), Convert.ToDecimal(s[4])));
+                }
+                if (s.Length == 4)
+                {
+                    int number;
+                    if (Int32.TryParse(s[2], out number))
+                    {
+                        clientList.Add(new Organization(s[0], Convert.ToDateTime(s[1]),
+                                                     number, Convert.ToDecimal(s[3])));
+                    }
+                    else
+                    {
+                        clientList.Add(new Investor(s[0], Convert.ToDateTime(s[1]),
+                                  Convert.ToDecimal(s[2]), Convert.ToDouble(s[3])));
+                    }
+                }
+            }
+            sr.Close();
+
+            foreach (Client client in clientList)
+            {
+                client.PrintInfo(richTextBox1);
+                richTextBox1.Text += "\n\n";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Client> clientList = new List<Client>();
+            StreamReader sr = new StreamReader("ClientList.txt", Encoding.UTF8);
+            while (!sr.EndOfStream)
+            {
+                string[] s = sr.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                if (s.Length == 5)
+                {
+                    clientList.Add(new Creditor(s[0], Convert.ToDateTime(s[1]), Convert.ToDecimal(s[2]),
+                                                      Convert.ToDouble(s[3]), Convert.ToDecimal(s[4])));
+                }
+                if (s.Length == 4)
+                {
+                    int number;
+                    if (Int32.TryParse(s[2], out number))
+                    {
+                        clientList.Add(new Organization(s[0], Convert.ToDateTime(s[1]),
+                                                     number, Convert.ToDecimal(s[3])));
+                    }
+                    else
+                    {
+                        clientList.Add(new Investor(s[0], Convert.ToDateTime(s[1]),
+                                  Convert.ToDecimal(s[2]), Convert.ToDouble(s[3])));
+                    }
+                }
+            }
+            sr.Close();
+            DateTime askDate;
+            if (!DateTime.TryParse(textBox1.Text, out askDate))
+                MessageBox.Show("Некорректный ввод даты!");
+            else
+            {
+                askDate = DateTime.Parse(textBox1.Text);
+                int foundClients = 0;
+                richTextBox1.Text = "";
+                foreach (Client client in clientList)
+                {
+                    if (client.IsClientByDate(askDate))
+                    {
+                        client.PrintInfo(richTextBox1);
+                        foundClients++;
+                    }
+                }
+                if (foundClients == 0)
+                {
+                    richTextBox1.Text = "Совпадений нет...";
+                }
+            }
+        }
+       public abstract class Client
+        {
+            public abstract void PrintInfo(RichTextBox richTextBox);
+            public abstract bool IsClientByDate(DateTime date);
+        }
+
+        public class Investor : Client
+        {
+            public string Surname { get; set; }
+            public DateTime DepositDate { get; set; }
+            public decimal DepositAmount { get; set; }
+            public double DepositInterest { get; set; }
+
+            public Investor(string surname, DateTime depositDate, decimal depositAmount, double depositInteres)
+            {
+                Surname = surname;
+                DepositDate = depositDate;
+                DepositAmount = depositAmount;
+                DepositInterest = depositInteres;
+            }
+
+
+            public override void PrintInfo(RichTextBox richTextBox)
+            {
+                richTextBox.Text += $"Фамилия вкладчика: {Surname}\n";
+                richTextBox.Text += $"Дата открытия вклада: {DepositDate.ToShortDateString()}\n";
+                richTextBox.Text += $"Размер вклада: {DepositAmount}" + " рублей\n";
+                richTextBox.Text += $"Процент по вкладу: {DepositInterest}" + "%\n";
+            }
+
+            public override bool IsClientByDate(DateTime date)
+            {
+                if (DepositDate == date)
+                    return true;
+                return false;
+            }
+        }
+
+        public class Creditor : Client
+        {
+            public string Surname { get; set; }
+            public DateTime CreditDate { get; set; }
+            public decimal CreditAmount { get; set; }
+            public double CreditInterest { get; set; }
+            public decimal CreditBalance { get; set; }
+
+            public Creditor(string surname, DateTime creditDate, decimal creditAmount, double creditInterest,
+                            decimal creditBalance)
+            {
+                Surname = surname;
+                CreditDate = creditDate;
+                CreditAmount = creditAmount;
+                CreditInterest = creditInterest;
+                CreditBalance = creditBalance;
+            }
+
+            public override void PrintInfo(RichTextBox richTextBox)
+            {
+                richTextBox.Text += $"Фамилия кредитора: {Surname}\n";
+                richTextBox.Text += $"Дата выдачи кредита: {CreditDate.ToShortDateString()}\n";
+                richTextBox.Text += $"Размер кредита: {CreditAmount}" + " рублей\n";
+                richTextBox.Text += $"Процент по кредиту: {CreditInterest}" + "%\n";
+                richTextBox.Text += $"Остаток долга: {CreditBalance}" + " рублей\n";
+            }
+
+            public override bool IsClientByDate(DateTime date)
+            {
+                if (CreditDate == date)
+                    return true;
+                return false;
+            }
+        }
+
+        public class Organization : Client
+        {
+            public string Name { get; set; }
+            public DateTime AccountDate { get; set; }
+            public int AccountNumber { get; set; }
+            public decimal AccountAmount { get; set; }
+
+            public Organization(string name, DateTime accountDate, int accountNumber, decimal accountAmount)
+            {
+                Name = name;
+                AccountDate = accountDate;
+                AccountNumber = accountNumber;
+                AccountAmount = accountAmount;
+            }
+
+            public override void PrintInfo(RichTextBox richTextBox)
+            {
+                richTextBox.Text += $"Название организации: {Name}\n";
+                richTextBox.Text += $"Дата открытия счета: {AccountDate.ToShortDateString()}\n";
+                richTextBox.Text += $"Номер счета: {AccountNumber}\n";
+                richTextBox.Text += $"Сумма на счету: {AccountAmount}" + " рублей\n";
+            }
+
+            public override bool IsClientByDate(DateTime date)
+            {
+                if (AccountDate == date)
+                    return true;
+                return false;
+            }
+        }
+    }
+}
